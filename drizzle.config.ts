5833c06f52ae02fdba7e3fbf81cf56927078fa8c
Teklez <zemenumekiria@gmail.com>
2025-08-10 20:49:42 +0300

    add drizzle and postgres

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+import { readConfig } from "./src/config";
+
+const dbUrl = readConfig()?.dbUrl ?? "";
+
+export default defineConfig({
+  schema: "home/zemen/Software-Development/projects/Gator/src/db/schema.ts",
+  out: "home/zemen/Software-Development/projects/Gator/src/db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: dbUrl,
+  },
+});
